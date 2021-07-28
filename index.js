7f9e63fa70cb4414913f6ddc605cb66fb22feb60
facuvelasco <velascofacundo@gmail.com>
2021-07-28 00:25:23 -0300

    404 message, and lock

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "build")));
 
+app.use("/api", (req, res) => {
+  res.sendStatus(404);
+});
+
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "build/index.html"));
 });
@@ -21,4 +25,4 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
 
-app.listen(3001, () => console.log("Servidor escuchando en el puerto 3000"));
+app.listen(3000, () => console.log("Servidor escuchando en el puerto 3000"));
