5af6b0286b165d6e924b015df94421a2a5fa732c
facuvelasco <velascofacundo@gmail.com>
2021-07-28 00:17:58 -0300

    base workshop

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const volleyball = require("volleyball");
+const path = require("path");
+
+// logging middleware
+app.use(volleyball);
+
+// parsing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
+// error middleware -> https://expressjs.com/es/guide/error-handling.html
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
+
+app.listen(3001, () => console.log("Servidor escuchando en el puerto 3000"));
