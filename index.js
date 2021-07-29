dc3b65ccc5f226a0840a564b6991bb929224362d
facuvelasco <velascofacundo@gmail.com>
2021-07-28 21:25:46 -0300

    index update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const volleyball = require("volleyball");
-const path = require("path");
 
 // logging middleware
 app.use(volleyball);
@@ -9,18 +8,14 @@ app.use(volleyball);
 // parsing middleware
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
-
 app.use("/api", (req, res) => {
   res.sendStatus(404);
 });
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
-});
-
 // error middleware -> https://expressjs.com/es/guide/error-handling.html
 app.use((err, req, res, next) => {
+  console.log("ERROR");
+  console.log(err);
   res.status(500).send(err.message);
 });
 
